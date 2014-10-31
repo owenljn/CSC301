fe7c9672d3f95a8f23950518758fecb79a514023
a-gvo <alina.gvo@gmail.com>
2014-10-31 17:07:19 -0400

    Added some more skeleton code, as well as implemented PayloadDescriptor and PayloadAction classes

--- Android/app/src/main/java/com/utoronto/timemng/descriptor/PayloadDescriptor.java
+++ Android/app/src/main/java/com/utoronto/timemng/descriptor/PayloadDescriptor.java
@@ -1,10 +1,40 @@
 package com.utoronto.timemng.descriptor;
 
 /**
- * 31/10/2014 15:30.
+ * Contains the type of action (e.g. create, delete, update event), and the month.
  */
 public class PayloadDescriptor {
-    public PayloadDescriptor() {
-        super();
+    private final int payloadAction;
+    private final Month month;
+
+    /**
+     * The default constructor.
+     * @param payloadAction the integer value for payload action.
+     * @param month         the Month object.
+     */
+    public PayloadDescriptor(final int payloadAction, final Month month) {
+        if (null != month) {
+            this.payloadAction = payloadAction;
+            this.month = month;
+        } else {
+            throw new IllegalArgumentException("Cannot construct PayloadDescriptor with null" +
+                    "month");
+        }
+    }
+
+    /**
+     * Gets the payload action.
+     * @return  the integer value for payload action.
+     */
+    public int getPayloadAction() {
+        return this.payloadAction;
+    }
+
+    /**
+     * Gets the month for the action.
+     * @return  the Month object.
+     */
+    public Month getMonth() {
+        return this.month;
     }
 }
