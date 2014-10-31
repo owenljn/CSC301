277cad115f624073d871180277fd7b96a5615375
hob19930414 <hob19930414@gmail.com>
2014-10-31 15:17:38 -0400

    Skelton
    Skelton

--- Android/app/src/main/java/com/utoronto/timemng/handler/CreateTaskPayloadHandler.java
+++ Android/app/src/main/java/com/utoronto/timemng/handler/CreateTaskPayloadHandler.java
@@ -1,5 +1,8 @@
 package com.utoronto.timemng.handler;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
@@ -19,7 +22,15 @@ public class CreateTaskPayloadHandler implements PayloadHandler {
     @Override
     public void execute(final Context context) {
         Log.d(TAG, "Received Message: " + this.payload);
-
+        
+        //ArrayList<String> parameters = new ArrayList<String>();
+       // Scanner src = new Scanner(payload);
+        //src.useDelimiter(";");
+        //while (src.hasNext()) {
+        //	parameters.add(src.next());
+        //}
+        //Log.d(TAG, parameters);
+        
         // show temporary message on the screen.... delete this after
         Toast.makeText(context, "MSG: " + this.payload, Toast.LENGTH_LONG).show();
 
--- Android/app/src/main/java/com/utoronto/timemng/handler/PayloadFactory.java
+++ Android/app/src/main/java/com/utoronto/timemng/handler/PayloadFactory.java
@@ -8,8 +8,17 @@ public class PayloadFactory {
 
     public PayloadHandler create(final String payload) {
         // TODO: 1. De-serialize payload into a POJO class "PayloadDescriptor.class"
-
+    	//if (payload[0] == 'C'){
+    	return new CreateTaskPayloadHandler(payload);
+   /* 	//}else if (payload[0] == 'U'){
+         //   return new UpdateTaskPayloadHandler(payload);
+    	}else if (payload[0] == 'D'){
+            return new DeleteTaskPayloadHandler(payload);
+    	}else{
+    		return null;
+    	}
+*/
         // TODO: provide different handlers based on the content of the payload...
-        return new CreateTaskPayloadHandler(payload);
+
     }
 }
