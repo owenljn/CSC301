a7cd8ebbd563701d96922fc6cf0212e187295d9b
a-gvo <alina.gvo@gmail.com>
2014-10-31 19:22:36 -0400

    Implemented Year class

--- Android/app/src/main/java/com/utoronto/timemng/descriptor/Year.java
+++ Android/app/src/main/java/com/utoronto/timemng/descriptor/Year.java
@@ -1,7 +1,38 @@
 package com.utoronto.timemng.descriptor;
 
+import android.util.Log;
+
+import java.util.Collection;
+
 /**
- * 31/10/2014 18:33.
+ * Contains the information for the specified year.
  */
 public class Year {
+    private static final String TAG = "Year";
+    private final int year;
+    private final Collection<Month> months;
+
+    /**
+     * The default constructor for this class.
+     * @param year          The year in question.
+     * @param whichMonths   A collection of months. Cannot be null or empty.
+     */
+    public Year(final int year, final Collection<Month> whichMonths) {
+        if (null != whichMonths && !whichMonths.isEmpty()) {
+            this.year = year;
+            this.months = whichMonths;
+        } else {
+            Log.i(TAG, "List of months is either null or empty.");
+            throw new IllegalArgumentException("The list of months cannot be null," +
+                    "and must not be empty");
+        }
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public Collection<Month> getMonths() {
+        return this.months;
+    }
 }
