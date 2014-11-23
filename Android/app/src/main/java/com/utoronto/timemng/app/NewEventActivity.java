c8dc6d9a997b48dd15311b42b7b99bc139e709ba
a-gvo <alina.gvo@gmail.com>
2014-11-23 17:46:52 -0500

    Implemented event adding

--- Android/app/src/main/java/com/utoronto/timemng/app/NewEventActivity.java
+++ Android/app/src/main/java/com/utoronto/timemng/app/NewEventActivity.java
@@ -6,11 +6,10 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.TextView;
-import com.utoronto.timemng.event.EventDto;
+import android.widget.*;
+import com.utoronto.timemng.server.ServerHelper;
 
 import java.util.Calendar;
 
@@ -18,8 +17,17 @@ import java.util.Calendar;
  * Add new event option.
  */
 public class NewEventActivity extends Activity {
-    private EventDto eventDto;
+    private static final String TAG = "c2dm add event";
     private boolean isAllDay = false;
+    private String eventTitle;
+    private String startDateVar;
+    private String endDateVar;
+    private String startTimeVar;
+    private String endTimeVar;
+    private String location;
+    private String invitees;
+    private String note;
+    private Calendar calendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -63,16 +71,15 @@ public class NewEventActivity extends Activity {
      * @param myView  the view that was clicked.
      */
     public void onStartDateClicked(final View myView) {
-        final Calendar calendar = Calendar.getInstance();
         final TextView endDate = (TextView) this.findViewById(R.id.end_date);
         final DialogFragment newFragment = new DatePickerFragment() {
             @Override
             public void onDateSet(final DatePicker view, final int year, final int monthOfYear, final int dayOfMonth) {
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, monthOfYear);
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                final String weekDay = DateFormat.format("EEEE", calendar).toString();
-                final String monthStr = DateFormat.format("MMM", calendar).toString();
+                NewEventActivity.this.calendar.set(Calendar.YEAR, year);
+                NewEventActivity.this.calendar.set(Calendar.MONTH, monthOfYear);
+                NewEventActivity.this.calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                final String weekDay = DateFormat.format("EEEE", NewEventActivity.this.calendar).toString();
+                final String monthStr = DateFormat.format("MMM", NewEventActivity.this.calendar).toString();
                 final String dateStr = weekDay + ", " + dayOfMonth + " " + monthStr + " " + year;
                 ((TextView) myView).setText(dateStr);
                 endDate.setText(dateStr);
@@ -83,28 +90,60 @@ public class NewEventActivity extends Activity {
 
     /**
      * The start time is clicked.
-     * @param view  the view that was clicked.
+     * @param myView  the view that was clicked.
      */
-    public void onStartTimeClicked(final View view) {
-        final DialogFragment newFragment = new TimePickerFragment();
+    public void onStartTimeClicked(final View myView) {
+        final TextView endTime = (TextView) this.findViewById(R.id.event_end_time);
+        final DialogFragment newFragment = new TimePickerFragment() {
+            @Override
+            public void onTimeSet(final TimePicker view, final int hourOfDay, final int minute) {
+                NewEventActivity.this.calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                NewEventActivity.this.calendar.set(Calendar.MINUTE, minute);
+                final String timeStrStart = DateFormat.format("HH:mm", NewEventActivity.this.calendar).toString();
+                NewEventActivity.this.calendar.add(Calendar.HOUR_OF_DAY, 1);
+                final String timeStrEnd = DateFormat.format("HH:mm", NewEventActivity.this.calendar).toString();
+                ((TextView)myView).setText(timeStrStart);
+                endTime.setText(timeStrEnd);
+            }
+        };
         newFragment.show(getFragmentManager(), "startTimePicker");
     }
 
     /**
      * The start date view is clicked.
-     * @param view  the view that was clicked.
+     * @param myView  the view that was clicked.
      */
-    public void onEndDateClicked(final View view) {
-        final DialogFragment newFragment = new DatePickerFragment();
+    public void onEndDateClicked(final View myView) {
+        final DialogFragment newFragment = new DatePickerFragment() {
+            @Override
+            public void onDateSet(final DatePicker view, final int year, final int monthOfYear, final int dayOfMonth) {
+                NewEventActivity.this.calendar.set(Calendar.YEAR, year);
+                NewEventActivity.this.calendar.set(Calendar.MONTH, monthOfYear);
+                NewEventActivity.this.calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                final String weekDay = DateFormat.format("EEEE", NewEventActivity.this.calendar).toString();
+                final String monthStr = DateFormat.format("MMM", NewEventActivity.this.calendar).toString();
+                final String dateStr = weekDay + ", " + dayOfMonth + " " + monthStr + " " + year;
+                ((TextView) myView).setText(dateStr);
+            }
+        };
         newFragment.show(getFragmentManager(), "endDatePicker");
     }
 
     /**
      * The start time is clicked.
-     * @param view  the view that was clicked.
+     * @param myView  the view that was clicked.
      */
-    public void onEndTimeClicked(final View view) {
-        final DialogFragment newFragment = new TimePickerFragment();
+    public void onEndTimeClicked(final View myView) {
+        final DialogFragment newFragment = new TimePickerFragment() {
+            @Override
+            public void onTimeSet(final TimePicker view, final int hourOfDay, final int minute) {
+                NewEventActivity.this.calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                NewEventActivity.this.calendar.set(Calendar.MINUTE, minute);
+                final String timeStrEnd = DateFormat.format("HH:mm", NewEventActivity.this.calendar).toString();
+                NewEventActivity.this.calendar.add(Calendar.HOUR_OF_DAY, 1);
+                ((TextView)myView).setText(timeStrEnd);
+            }
+        };
         newFragment.show(getFragmentManager(), "endTimePicker");
     }
 
@@ -119,9 +158,39 @@ public class NewEventActivity extends Activity {
     /**
      * Create event by filling out all the relevant fields and sending request
      * to server.
-     * @param view  the button clicked.
+     * @param myView  the button clicked.
      */
-    public void onCreateEventClicked(final View view) {
+    public void onCreateEventClicked(final View myView) {
+        final EditText titleView = (EditText) this.findViewById(R.id.event_title_input);
+        this.eventTitle = titleView.getText().toString();
+        if (null != this.eventTitle) {
+            final TextView startDateView = (TextView) this.findViewById(R.id.start_date);
+            final TextView endDateView = (TextView) this.findViewById(R.id.end_date);
+            final TextView startTimeView = (TextView) this.findViewById(R.id.event_start_time);
+            final TextView endTimeView = (TextView) this.findViewById(R.id.event_end_time);
+            final EditText locationView = (EditText) this.findViewById(R.id.event_location_input);
+            final EditText inviteesView = (EditText) this.findViewById(R.id.event_add_ppl_input);
+            final EditText noteView = (EditText) this.findViewById(R.id.event_note_input);
+            this.startDateVar = startDateView.getText().toString();
+            this.endDateVar = endDateView.getText().toString();
+            this.startTimeVar = startTimeView.getText().toString();
+            this.endTimeVar = endTimeView.getText().toString();
+            this.location = locationView.getText().toString();
+            this.invitees = inviteesView.getText().toString();
+            this.note = noteView.getText().toString();
+            Log.i(TAG, "is all day: " + Boolean.toString(this.isAllDay));
+            Log.i(TAG, "title: " + this.eventTitle);
+            Log.i(TAG, "start date: " + this.startDateVar);
+            Log.i(TAG, "end date: " + this.endDateVar);
+            Log.i(TAG, "start time: " + this.startTimeVar);
+            Log.i(TAG, "end time: " + this.endTimeVar);
+            Log.i(TAG, "location: " + this.location);
+            Log.i(TAG, "invitees: " + this.invitees);
+            Log.i(TAG, "note: " + this.note);
+            ServerHelper.sentToServer();
+        } else {
+            // TODO: pop up saying there's an error.
+        }
     }
 
     /**
@@ -129,20 +198,19 @@ public class NewEventActivity extends Activity {
      */
     private void setDefaultDateAndTime() {
         final Intent intent = getIntent();
-        final Calendar calendar = Calendar.getInstance();
         final int day = Integer.parseInt(intent.getStringExtra("day"));
         final int month = Integer.parseInt(intent.getStringExtra("month"));
         final int year = Integer.parseInt(intent.getStringExtra("year"));
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.YEAR, year);
-        final CharSequence beginTime = DateFormat.format("HH:" + "00", calendar).toString();
-        calendar.add(Calendar.HOUR, 1);
-        final CharSequence finishTime = DateFormat.format("HH:" + "00", calendar).toString();
-        final CharSequence monthStr = DateFormat.format("MMM", calendar).toString();
-        final CharSequence dayStr = DateFormat.format("dd", calendar).toString();
-        final CharSequence weekDay = DateFormat.format("EEEE", calendar).toString();
-        final CharSequence yearStr = DateFormat.format("yyyy", calendar).toString();
+        this.calendar.set(Calendar.DAY_OF_MONTH, day);
+        this.calendar.set(Calendar.MONTH, month);
+        this.calendar.set(Calendar.YEAR, year);
+        final CharSequence beginTime = DateFormat.format("HH:" + "00", this.calendar).toString();
+        this.calendar.add(Calendar.HOUR, 1);
+        final CharSequence finishTime = DateFormat.format("HH:" + "00", this.calendar).toString();
+        final CharSequence monthStr = DateFormat.format("MMM", this.calendar).toString();
+        final CharSequence dayStr = DateFormat.format("dd", this.calendar).toString();
+        final CharSequence weekDay = DateFormat.format("EEEE", this.calendar).toString();
+        final CharSequence yearStr = DateFormat.format("yyyy", this.calendar).toString();
         final String dateString = String.format("%s, %s %s %s", weekDay, dayStr, monthStr, yearStr);
         final TextView startDate = (TextView) this.findViewById(R.id.start_date);
         final TextView startTime = (TextView) this.findViewById(R.id.event_start_time);
---
+++ Android/app/src/main/java/com/utoronto/timemng/server/ServerHelper.java
@@ -0,0 +1,10 @@
+package com.utoronto.timemng.server;
+
+/**
+ * 23/11/2014 17:08.
+ */
+public class ServerHelper {
+    public static void sentToServer() {
+        // TODO: Implement sending to server.
+    }
+}
